2a2da327540277d7c02c8123c18b6767748a13c3
Viraj Nimbalkar <108717558+veris-viraj@users.noreply.github.com>
2022-07-12 15:27:28 +0530

    Function Types and Props, Types Model, Submit Todos

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,11 +4,12 @@ import "./styles.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>; //Copy from App.tsx by hovering on setTodo
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input
         type="input"
         value={todo}
