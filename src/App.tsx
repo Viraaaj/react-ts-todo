6ff1c4d53ba17a2fbb7bd50b917cbd0f074c29f5
Viraj Nimbalkar <108717558+veris-viraj@users.noreply.github.com>
2022-07-12 15:13:36 +0530

    Input UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <div className="App">Hello World!</div>;
-}
+import "./App.css";
+import InputField from "./components/InputField";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Task-Sheet</span>
+
+      <InputField />
+    </div>
+  );
+};
 
 export default App;
